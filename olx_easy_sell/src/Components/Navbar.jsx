5d7ecc12fb2814bcf9ba6dcfde0ae53e300d8d91
fw20-0769 <tarakkarmakar1434@gmail.com>
2022-11-10 18:49:52 +0530

    Day_2

--- olx_easy_sell/src/Components/Navbar.jsx
+++ olx_easy_sell/src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Show,Select, Hide ,Box,Stack, Flex,Input} from '@chakra-ui/react'
+import { Show,Select, Hide ,Box,Stack, Flex,Input,InputGroup,InputRightAddon,InputLeftAddon, InputRightElement} from '@chakra-ui/react'
 import { SearchIcon,ChevronDownIcon,ChatIcon,BellIcon} from '@chakra-ui/icons'
 import  styles from "./Navbar.css"
 
@@ -38,40 +38,36 @@ return (
     <Flex className='large_nav_flex'>
 
 <Box><img className='logo_nav' src="https://scontent.fccu13-1.fna.fbcdn.net/v/t39.30808-6/315002987_1801734266854939_7038530819874117418_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=mZOnteQGlIoAX9QsIFl&_nc_ht=scontent.fccu13-1.fna&oh=00_AfB5j1POvfVtG1MsVSxa4Cb3hXcs_N9s3eqlqXDigOUYFQ&oe=6370BB18"></img></Box>
-<Box className='first_option_nav'> 
+<Box  className='first_option_nav'>
+<InputGroup   >
+    <InputLeftAddon children={<SearchIcon />} style={{border:"none",background:"white"}} />
+    <Input type='text'   value="India" width={300} />
+    <InputRightAddon children={<select style={{border:"none",background:'white' ,width:"30px"}}>
+        <option ></option>
+        <option value="Ind">India</option>
+        <option> USA</option>
+    </select>}/>
 
-<Flex>
+  </InputGroup>
+  </Box>
 
-
-<SearchIcon w={30}/>
-
-<input value="India" className='India_Input'/>
-<select value="" >
-<option><ChevronDownIcon/></option>
-    <option value="India">India</option>
-    <option value="USA">USA</option>
-
-
-</select>
-
-
-</Flex>
-
-    </Box>
     <Box className='search_input_nav'>
         <Input className='search_inp'></Input>
-    <Button className='searh_click_nav'><SearchIcon /></Button>
+    <button  className='searh_click_nav'><SearchIcon /></button>
     </Box>
     <Box><select name="select_language" id="lang">
         <option value="ENG"><h1>ENGLISH</h1></option>
-        <option value="ENG"><h1>हिंदी</h1></option>
+        <option value="Hindi"><h1>हिंदी</h1></option>
 
     </select>
     </Box>
-    <Box ><button ><ChatIcon/></button></Box>
-    <Box><button><BellIcon /></button></Box>
-    <Box></Box>
-    <Box></Box>
+    <Box ><button ><ChatIcon w={6} h={6}/></button></Box>
+    <Box><button><BellIcon w={6} h={6} /></button></Box>
+    <Box><Button colorScheme="white" color="black">Login</Button></Box>
+
+    <Button colorScheme='teal' size='lg'>
+   Sell
+  </Button>
 
     </Flex>
 </Stack>
